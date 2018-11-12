afe059ea7aa0d235aff6cdc4f709a26b932e1fc3
Pedro Villamor Lopez <pedro88vl@gmail.com>
2018-11-12 17:00:25 +0000

    controllers updated

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,50 @@
+class MatchesController < ApplicationController
+  before_action :set_match, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @matchs = Match.all
+  end
+
+  def show
+  end
+
+  def new
+    @match = Match.new
+  end
+
+  def edit
+  end
+
+  def create
+    @match = Match.new(match_params)
+
+    if @match.valid?
+      @match.save
+      redirect_to match_path(@match)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @match.update(match_params)
+      redirect_to match_path(@match)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @match.destroy
+  end
+
+  private
+
+  def set_match
+    @match = Match.find(params[:id])
+  end
+
+  def match_params
+    params.require(:match).permit(:user, :job)
+  end
+end
