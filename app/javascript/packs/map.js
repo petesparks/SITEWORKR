cd018f9894b72fe37faf2d47bb1fda5d1907bbd1
peter sparks <petersparks18@gmail.com>
2018-11-15 16:10:14 +0000

    autocomplete finally works

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -3,6 +3,14 @@ import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 
 const mapElement = document.getElementById('map');
 
+const addressInput = document.getElementById('search2');
+
+if (addressInput) {
+  const places = require('places.js');
+  const placesAutocomplete = places({
+    container: addressInput
+  });
+}
 
 if (mapElement) { // only build a map if there's a div#map to inject into
   mapboxgl.accessToken = process.env.MAPBOX_API_KEY; // API key from `.env`
