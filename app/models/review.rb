b5bf57e7a28106e55cd3fb4fbabfff01978ade81
peter sparks <petersparks18@gmail.com>
2018-11-22 12:07:35 +0000

    review models generated

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :match
+  belongs_to :user
+  validates_presence_of :content, :match_id, :user_id
   validates :content, length: { maximum: 400 }
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, presence: true, numericality: { only_integer: true }
 end
