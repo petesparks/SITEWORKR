e7daa4aa3c2da0cd68c54ae6a7ce7bc0a179bcda
Pedro Villamor Lopez <pedro88vl@gmail.com>
2018-11-27 12:33:32 +0000

    flatpickr uncommented

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,14 +1,14 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/airbnb.css" // Note this is important!
 
-// import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-// flatpickr("#start_date", {
-//   altInput: true,
-//   plugins: [new rangePlugin({ input: "#end_date"})],
-//   disable: JSON.parse(document.querySelector(".user-card").dataset.dates),
-//   inline: true
-// })
+flatpickr("#start_date", {
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#end_date"})],
+  disable: JSON.parse(document.querySelector(".user-card").dataset.dates),
+  inline: true
+})
 
 
 
