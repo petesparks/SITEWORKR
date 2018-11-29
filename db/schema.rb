24fdca7a1d0c904cd6f2d48d548de1c9eec897cf
pedroVL <43155634+pedroVL@users.noreply.github.com>
2018-11-29 18:51:52 +0000

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_132831) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.text "content"
+    t.string "content"
     t.integer "rating"
     t.bigint "match_id"
     t.bigint "user_id"
